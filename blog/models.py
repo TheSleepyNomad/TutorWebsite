43e159f7ba66630f71d600f2cfce67b6f4b47abb
Ilya Pankov <iluxaan@mail.ru>
2022-07-12 17:52:27 +0300

    feat: add media root / write test models for blog

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,47 @@
-from distutils.command.upload import upload
+from turtle import update
+from unicodedata import category
 from django.db import models
 
 
 # Create your models here.
-class Article(models.Model):
-    pass
-    # image = model.ImageField(upload_to='images')
-
-
 class Category(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Наименование категории')
+
+    def __str__(self) -> str:
+        return self.name
 
 
 class Tag(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='Наименование тэга')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Gallery(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Наименование файла')
+    image = models.ImageField(upload_to='images')
+    description = models.TextField(verbose_name='Описание изображения', blank=True)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=70, verbose_name='Заголовок')
+    entry_image = models.ImageField(upload_to='blogs_banners')
+    # Хранит в себе HTML разметку с текстом
+    text = models.TextField()
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # created_by = models.ForeignKey()
+
+
+# class Comment(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     post = models.ForeignKey(Article, on_delete=models.CASCADE)
+#     text = models.TextField()
+#     created = models.DateTimeField(default=timezone.now, null=True)
+#     moderation = models.BooleanField(default=False)
+
+#     def __str__(self):
+#         return self.text
--- tutorweb/urls.py
+++ tutorweb/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
-from landingpage.views import index
+from django.conf import settings  
+from django.conf.urls.static import static  
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('landingpage.urls')),
     path('blogs/', include('blog.urls')),
 ]
+
+if settings.DEBUG:  
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)  
