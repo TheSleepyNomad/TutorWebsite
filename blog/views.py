462afb6d82acf21349f375304dae8605faf20d61
Ilya Pankov <iluxaan@mail.ru>
2022-07-12 09:53:12 +0300

    feat: add urls for blog app and write simple view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
+def blog_list(request):
+    return render(request,'blog/blog.html')
+
+def blog_detail(request):
+    return render(request,'blog/blog_detail.html')
--- tutorweb/urls.py
+++ tutorweb/urls.py
@@ -20,5 +20,6 @@ from landingpage.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('blogs/', include('blog.urls')),
 ]
