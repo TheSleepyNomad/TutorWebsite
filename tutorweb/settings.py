0e4193cc13fa3957e87c22b5d5e431e99897a2c7
Ilya Pankov <iluxaan@mail.ru>
2022-07-12 11:24:32 +0300

    docs,feat: upd req.txt / add .env / hide sec info

--- tutorweb/settings.py
+++ tutorweb/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dgu0jypp!sofomya8wpk^^hkam5m@br$i+_o297s1wk0_$j68@'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -123,7 +124,6 @@ STATICFILES_DIRS = [
     Path(BASE_DIR, 'static'),
     
 ]
-print(Path(BASE_DIR, 'static'))
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
