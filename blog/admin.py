67a984af03ddfc988d59b5101544ed79ea35e2f5
Ilya Pankov <iluxaan@mail.ru>
2022-07-13 10:19:43 +0300

    feat: reg blog model in django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
-
+from .models import Article, Category, Tag, Gallery
 # Register your models here.
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'entry_image', 'text', 'category', 'created_at', 'updated_at')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Gallery)
+class GalleryAdmin(admin.ModelAdmin):
+    pass
