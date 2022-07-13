b19d4c38eded9313ca54a376f96474cd93214128
Ilya Pankov <iluxaan@mail.ru>
2022-07-13 11:35:31 +0300

    feat: test models in html

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', blog_list, name='blog_list'),
-    path('blog_detail/', blog_detail, name='blog_detail'),
+    path('blog_detail/<int:pk>', blog_detail, name='blog_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
 import re
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 def blog_list(request):
     return render(request,'blog/blog.html')
 
-def blog_detail(request):
-    return render(request,'blog/blog_detail.html')
+def blog_detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    return render(request,'blog/blog_detail.html', context={"article" : article})
