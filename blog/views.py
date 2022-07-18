b2302c5f30b2b76de67f959a2e6564d23a743e8c
Ilya Pankov <iluxaan@mail.ru>
2022-07-18 15:14:44 +0300

    refactor, style: del old usefull code and add commits

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
-from sys import prefix
 from django.shortcuts import render
 from .models import Article, Gallery, Tag, Category
 from django.http import JsonResponse
-from datetime import datetime
 from django.db.models import Q
 from django.db.models import Count
 from django.views.generic.detail import DetailView
@@ -18,8 +16,12 @@ class BlogsListView(ListView):
     template_name = 'blog/blog.html'
 
     def post(self, request, *args, **kwargs):
+        # Проверяем на fetch/ajax запрос
+        # На этой старнице только одна форма для добавления статей
         if is_fetch:
+            # Если количество отправленных файлов в пакете более 1, значит были файлы добавленные в саму статью
             if len(request.FILES) > 1:
+                # Проходимся по полученным файлам, проверяем на существование и создаем по необходимости
                 for img in range(1, len(request.FILES)):
                     image = request.FILES.get(f'gallary{img}')
                     if Gallery.objects.filter(prefix='other', image='images/' + str(image)).exists():
@@ -27,10 +29,12 @@ class BlogsListView(ListView):
                     else:
                         gallery_image = Gallery.objects.create(prefix='other', image=image)
                         gallery_image.save()
+            # Проверяем картинку-баннер
             if Gallery.objects.filter(prefix='entery', image='images/' + str(request.FILES.get('entry-img'))).exists():
                 entry_image = Gallery.objects.get(prefix='entery', image='images/' + str(request.FILES.get('entry-img')))
             else:
                 entry_image = Gallery.objects.create(prefix='entery', image=request.FILES.get('entry-img'))
+            # Создаем новую статью
             article = Article(
             title=request.POST.get('title'),
             prev_text=request.POST.get('prev_text'),
@@ -39,16 +43,20 @@ class BlogsListView(ListView):
             category=Category.objects.get(pk=request.POST.get('category')), # Todo Почитать документацию Django, возможно можно реализовать менее затратно
             )
             article.save()
+            # Так как отношение ManyToMany, то использует set и проходимся по списку id тэгов
             article.tags.set(Tag.objects.filter(pk__in=request.POST.getlist('tag')))
             return JsonResponse({'status':'200', 'ok': True})
         return self.get(request, *args, **kwargs)
 
 
     def get(self, request, *args, **kwargs):
+        # Обработка фильтров для статей
         search_article = request.GET.get('search')
         category_article = request.GET.get('category')
         tag_article = request.GET.get('tag')
+        # Через форму поиска
         if search_article:
+            # Поиск идет по превью тексту и заголовку
             self.queryset = Article.objects.filter(\
                 Q(title__icontains=search_article)\
                 | Q(prev_text__icontains=search_article)\
@@ -62,6 +70,7 @@ class BlogsListView(ListView):
                 .only('title', 'prev_text', 'entry_image', 'created_at').order_by('-id')
         if category_article:
             self.queryset = Article.objects.filter(category__name=category_article).only('title', 'prev_text', 'entry_image', 'created_at').order_by('-id')
+
         return super().get(request, *args, **kwargs)
 
 
@@ -72,14 +81,6 @@ class BlogsListView(ListView):
         context['recrent_articles'] = Article.objects.filter(created_at__date__lt=datetime.now()).only('title', 'entry_image', 'created_at').order_by('-id')[:5]
         return context
 
-def blog_detail(request, pk):
-    
-    context = {
-        'article': Article.objects.get(pk=pk),
-        'tags': Tag.objects.all().values_list('id', 'name'),
-        'category': Category.objects.all().values_list('id', 'name').annotate(article_count=Count('article'))
-    }
-    return render(request,'blog/blog_detail.html', context=context)
 
 class BlogDetailView(DetailView):
     model = Article
