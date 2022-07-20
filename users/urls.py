9b543af7c27689b779adac111a728d42e17dc926
Ilya Pankov <iluxaan@mail.ru>
2022-07-20 12:36:16 +0300

    feat: create login and reg fbv view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
+from users.views import login_view, registration_view
 app_name = 'users'
 
 urlpatterns = [
-    
+    path('registration/', registration_view, name='registration'),
+    path('login/', login_view, name='login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def registration_view(request):
+    return render(request, 'users/sign_up.html')
+
+def login_view(request):
+    return render(request, 'users/sign_in.html')
